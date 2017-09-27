59e6c4830cba13ff35ea05bb7bf7ade4774ebd4d
Mickael Matuchak <mickael.matuchak@etudiant.univ-lille1.fr>
2017-09-27 09:26:48 +0200

    Utilise ORM dans la classe Product

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -1,43 +1,34 @@
 <?php
 
-
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
 class Product
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
     private $price;
 
     /**
-     * @return mixed
+     * @ORM\Column(type="text")
      */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPrice()
-    {
-        return $this->price;
-    }
-
-    /**
-     * @param mixed $price
-     */
-    public function setPrice($price)
-    {
-        $this->price = $price;
-    }
+    private $description;
 }
